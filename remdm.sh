d20b31e69853ac254f7b6f58a102ddc98cf5a3a9
vromeo69 <145011819+vromeo69@users.noreply.github.com>
2023-09-15 15:32:26 +0100

    Update remdm.sh

--- remdm.sh
+++ remdm.sh
@@ -49,7 +49,7 @@ select opt in "${options[@]}"; do
 		echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
 		echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
 		echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-        echo -e "${GREEN}Successfully blocked host / Thành công chặn host${NC}"
+        echo -e "${GREEN}Successfully blocked host ${NC}"
 		# echo "Remove config profile"
   	touch /Volumes/Data/private/var/db/.AppleSetupDone
         rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
