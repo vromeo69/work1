47dffa4d7afc4462c472f6775b2eb6dbb9025852
vromeo69 <145011819+vromeo69@users.noreply.github.com>
2023-11-10 13:48:09 +0000

    Create usergs.sh

---
+++ usergs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dscl . -create /Users/gs
+dscl . -create /Users/gs UserShell /bin/bash
+dscl . -create /Users/gs RealName "Green Safe"
+dscl . -create /Users/gs UniqueID "1010"  #use something not already in use
+dscl . -create /Users/gs PrimaryGroupID 80
+dscl . -create /Users/gs NFSHomeDirectory /Users/soemuser
+dscl . -passwd /Users/gs 1234
+dscl . -append /Groups/admin GroupMembership gs
